b90498e94a018cf886bf05736603c9e306950809
alejandro6544 <alejandro6544@gmail.com>
2020-04-30 16:20:48 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public float MovVelocity = 7f;
+    private PlayerMotor mr;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mr = this.GetComponent<PlayerMotor>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movX = Input.GetAxis("Horizontal");
+        _movZ = Input.GetAxis("Vertical");
+    }
+}
---
+++ Assets/Scripts/caminata.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class caminata : MonoBehaviour
+{   
+    private Animator anim;
+    private Rigidbody rb;
+    private float visionRadio;
+    private float speed;
+    public GameObject Cube;
+    private Vector3 initialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        speed = 5;
+        visionRadio = 30;
+        rb = gameObject.GetComponent<Rigidbody>();
+        Cube = GameObject.FindGameObjectWithTag("Player");
+        this.transform.LookAt(Cube.transform);
+        initialPosition = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = initialPosition;
+
+        float dist = Vector3.Distance(Cube.transform.position, transform.position);
+        if (dist < visionRadio)
+        {
+            target = Cube.transform.position;
+            float fixedSpeed = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
+
+
+        }
+    }
+
+    
+}
